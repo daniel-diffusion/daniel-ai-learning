0430c9b40749c655215e8590740e26d0c29b2431
Daniel Balan <danielbalan@macpro.lan>
2026-01-05 16:56:00 +0100

    updated the prompt example

--- day5/day5_first_api_call.py
+++ day5/day5_first_api_call.py
@@ -6,9 +6,13 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+prompt = """
+You are phisycs teacher.
+Explain a blackhole in simple terms for a colleage student.
+"""
+
 response = client.responses.create(
     model="gpt-4.1-mini",
-    input="Explain API vs MCP in simple developer terms.",
+    input=prompt,
 )
-
 print(response.output_text)
